c08f9c0d69b3a9a072de424ad059edd13bbdc17d
Quanshu <2033581678@qq.com>
2023-10-13 21:26:36 +0800

    first commit

---
+++ src/main/java/com/quanshu/loveback/LoveBackApplication.java
@@ -0,0 +1,13 @@
+package com.quanshu.loveback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoveBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoveBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/quanshu/loveback/LoveBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quanshu.loveback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoveBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
